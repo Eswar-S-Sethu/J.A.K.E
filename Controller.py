0813007d15eaa7ef003814d0d9bc63e3b17fa365
Eswar-S-Sethu <eswarssethu2002@gmail.com>
2024-10-10 13:55:19 +1100

    added phone call and file conversion functions

---
+++ Controller.py
@@ -0,0 +1,3 @@
+'''This is meant to control all the others and keep the resource usage in check. It runs the threads, main.py,
+automations,etc and will have a KILL SWITCH to do an instant shutdown of the AI in case things go wrong as the things
+i try are experimental'''
